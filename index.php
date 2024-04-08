f7db980e714b28df494ee33e960e0696d45d0df2
Demetrius <Demetriuslewis2012@gmail.com>
2024-04-08 11:47:39 -0400

    feat: implement the Process Manager in Port Scanner scan utility

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 <?php
 
-require_once 'src/PortScanner/PortScanner.php';
+require_once './vendor/autoload.php';
+
+use Demetrius\PhpPortScanner\PortScanner\PortScanner;
 
 $port_scanner = new PortScanner();
 $host = '127.0.0.1';
 $starting_port = 1;
-$ending_port = 65535;
+$ending_port = 1000;
 
 $port_scanner->scan($host, $starting_port, $ending_port);
--- src/PortScanner/PortScanner.php
+++ src/PortScanner/PortScanner.php
@@ -1,31 +1,45 @@
 <?php
 
+namespace Demetrius\PhpPortScanner\PortScanner;
+
+use Demetrius\PhpPortScanner\ProcessManager\ProcessManager;
+
 class PortScanner 
 {
-    public function __construct() {
+    public function scan($host, $starting_port, $ending_port, $num_processes = 4) {
+        echo "Port scanner is running..." . PHP_EOL;
 
-        echo "Port scan is running..." . PHP_EOL;
-        
+        $port_ranges = $this->splitPortRange($starting_port, $ending_port, $num_processes);
+
+        ProcessManager::runConcurrently($num_processes, function () use ($host, $port_ranges) {
+            foreach ($port_ranges as $range) {
+                $this->scanPorts($host, $range['start'], $range['end']);
+            }
+        });
+
+        echo "Port scanner has finished." . PHP_EOL;
     }
-    
-    public function __destruct() {
 
-        echo "Port scan has finished." . PHP_EOL;
+    private function splitPortRange($start_port, $end_port, $num_processes) {
+        $port_ranges = [];
+        $range_size = ceil(($end_port - $start_port + 1) / $num_processes);
 
+        for ($i = 0; $i < $num_processes; $i++) {
+            $range_start = $start_port + ($i * $range_size);
+            $range_end = min($start_port + (($i + 1) * $range_size) - 1, $end_port);
+            $port_ranges[] = ['start' => $range_start, 'end' => $range_end];
+        }
+
+        return $port_ranges;
     }
 
-    public function scan($host = '127.0.0.1', $starting_port = 1, $ending_port = 65535) {
-
-        for ($port = $starting_port; $port <= $ending_port; $port++) {
+    private function scanPorts($host, $start_port, $end_port) {
+        for ($port = $start_port; $port <= $end_port; $port++) {
             $connected = @fsockopen($host, $port, $errno, $errstr, 30);
-
-            if (!empty($connected)) {
+            if ($connected !== false) {
                 echo "Port $port is open" . PHP_EOL;
                 fclose($connected);
             }
-
-            usleep(10000); // Add a delay so older cpus don't explode
         }
-
     }
 }
